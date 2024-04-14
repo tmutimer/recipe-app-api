c5990d8312db7aa92ce598a823e91136e9e69957
Tom Mutimer <tmutimer96@gmail.com>
2024-04-14 16:09:46 +0100

    Make email required

--- app/core/models.py
+++ app/core/models.py
@@ -11,9 +11,10 @@ from django.contrib.auth.models import (
 
 class UserManager(BaseUserManager):
     """Manager for users."""
-
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
+        if not email:
+            raise ValueError('User must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
